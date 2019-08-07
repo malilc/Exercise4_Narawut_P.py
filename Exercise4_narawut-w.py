789a82319b11357ee135a7cca823f1b8744e8d13
malilc <malilc@users.noreply.github.com>
2019-08-07 14:57:16 +0700

    score

---
+++ Exercise4_narawut-w.py
@@ -0,0 +1,9 @@
+FoundationEnglish=70.5
+GeneralBusiness=68.5
+IntroductiontoComputerSystems=80.5
+ComputerProgramming=78.8
+print("--- Your Score ---")
+print("Foundation English :",FoundationEnglish)
+print("General Business : ",GeneralBusiness)
+print("Introduction to Computer Systems : ",IntroductiontoComputerSystems)
+print("Computer Programming :",ComputerProgramming)
