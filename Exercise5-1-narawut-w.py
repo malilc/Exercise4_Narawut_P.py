c4b3f98532452eed3b712fc28cedb1a086c2a96d
malilc <malilc@users.noreply.github.com>
2019-08-07 15:46:19 +0700

    Exercise5-1
    + - * /

---
+++ Exercise5-1-narawut-w.py
@@ -0,0 +1,7 @@
+Num1 = int(input("plase enter your first number :\n"))
+Num2 = int(input("plase enter your second number2 :\n"))
+
+print(Num1,"+",Num2,"=",(Num1+Num2))
+print(Num1,"-",Num2,"=",(Num1-Num2))
+print(Num1,"*",Num2,"=",(Num1*Num2))
+print(Num1,"/",Num2,"=",(Num1/Num2))
